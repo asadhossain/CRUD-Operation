fdaca13ecd135f62f5f6ec30c88279cf2359365f
asadhossain <developerasad007@gmail.com>
2023-03-14 03:25:54 +0600

    a

--- function.php
+++ function.php
@@ -20,13 +20,11 @@
 
         }
 
-        public function add_data ($data)
-        {
+        public function add_data ($data){
             $std_name = $data["std_name"];
             $std_roll = $data["std_roll"];
             $std_img = $_FILES ["std_img"] ['name'];
             $tmp_name = $_FILES ['std_img'] ['tmp_name'];
-            $std_name = $data["std_name"];
 
             $query = "INSERT INTO students(std_name, std_roll, std_img) VALUE ('$std_name',$std_roll, '$std_img')";
 
