7ae7be7239f21a51ce654b18dc506fe492f81c40
asadhossain <developerasad007@gmail.com>
2023-03-15 02:05:40 +0600

    ASAD

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit_btn'])){
 }
 
 
-#$students = $objCrudAdmin->display_data();
+$students = $objCrudAdmin->display_data();
 
 
 ?>
@@ -64,18 +64,18 @@ if (isset($_POST['submit_btn'])){
             </tr>
         </thead>
         <tbody>
-            <?php# while ($students=mysqli_fetch_assoc($students)) { ?>
+            <?php while ($student=mysqli_fetch_assoc($students)) { ?>
             <tr>
-                <td><?php# echo $students['id']   ?></td>
-                <td><?php #echo $students['std_name']   ?></td>
-                <td><?php #echo $students['std_roll']   ?></td>
+                <td><?php echo $students['id']   ?></td>
+                <td><?php echo $students['std_name']   ?></td>
+                <td><?php echo $students['std_roll']   ?></td>
                 <td></td>
                 <td>
                     <a class= "btn btn-success " href="#">Edit</a>
                     <a class= "btn btn-warning" href="#">Delete</a>
                 </td>
              </tr>
-           <?php #  } ?>
+           <?php  } ?>
         </tbody>
 
     </table>
